5a84128397e5e6f34234c54716545842df6a8e7c
SERGIO GONZÁLEZ VELASCO <Sergio.Gonzalez35@alu.uclm.es>
2024-05-16 20:19:02 +0200

    Primer commit, creada la clase problema y el main encargados de la carga del json

---
+++ Lab1/lab1.py
@@ -0,0 +1,12 @@
+import json
+
+class Problema:
+    def __init__ (self, problema_json):
+        # Bloque de código encargado de cargar el json que ha sido recibido como parámetro
+        with open(problema_json, 'r') as f:
+            problema_json = json.load(f)
+
+
+#
+if __name__ == '__main__':
+    problema = Problema('test.json') # Inicializamos el problema cargando el json y parametrizandolo
