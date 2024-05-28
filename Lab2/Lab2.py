4ad29c6103a4fed45cffcaaa07cd54cf5614e340
SERGIO GONZÁLEZ VELASCO <Sergio.Gonzalez35@alu.uclm.es>
2024-05-28 09:39:55 +0200

    Ordenación de código, eliminación de elementos en métodos que al final no se han utilizado, añadidos comentarios y el método "estadísticas"

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -2,6 +2,8 @@ import json
 import numpy as np
 import os
 from draw_policy import *
+from time import time
+
 
 class Entorno:
     def __init__(self, entorno_json, penalizacion, pen_peligro, estocasticidad):
@@ -21,10 +23,6 @@ class Entorno:
         self.penalizacion_peligro = pen_peligro
         self.estocasticidad = estocasticidad
 
-    # Verifica si ese estado es de peligro
-    def es_peligro(self, estado):
-        return (estado.fila, estado.columna) in self.peligros
-
     # Verifica si ese estado está bloqueado
     def es_bloqueado(self, estado):
         return (estado.fila, estado.columna) in self.bloqueados
@@ -33,9 +31,11 @@ class Entorno:
     def es_valido(self, estado):
         return 0 <= estado.fila < self.filas and 0 <= estado.columna < self.columnas and not self.es_bloqueado(estado)
 
+    # Verifica si es estado de destino
     def es_destino(self, estado):
         return (estado.fila, estado.columna) in self.destinos or (estado.fila, estado.columna) in self.peligros_fatales
 
+    # Verifica si esa acción es válida
     def es_accion_valida(self, accion, estado):
         if accion == 0:
             return self.es_valido(Estado(estado.fila - 1, estado.columna))
@@ -55,10 +55,12 @@ class Entorno:
         nuevo_estado, recompensa = self.aplicar_accion(accion_elegida, estado_actual)
         return nuevo_estado, recompensa
 
+    # Método encargado de aplicar la acción, aquí se tiene en cuenta la estocasticidad del entorno
+    # Si el valor es superior a la estocasticidad, se aplica el correspondiente método
     def aplicar_accion(self, accion, estado):
         accion_elegida = accion
         estado_actual = estado
-
+        # Genera un valor entre 0 y 1, si es superior al valor de estocasticidad llamamos al susodicho método
         if np.random.rand() > self.estocasticidad:
             accion_elegida = self.aplicar_estocasticidad(accion)
 
@@ -84,6 +86,7 @@ class Entorno:
             recompensa = self.obtener_recompensa(nuevo_estado)
         return nuevo_estado, recompensa
 
+    # Método encargado de la estocasticidad, se aplica a las perpendiculares, tal como viene definido en los apuntes
     def aplicar_estocasticidad(self, accion):
         movimientos = [0, 1, 2, 3]
         if accion == movimientos[0]:  # Si Arriba
@@ -110,6 +113,8 @@ class Entorno:
             else:
                 return movimientos[0]  # Arriba
 
+    # Obtención de las recompensas asociadas a cada estado, obtenidas de las variables en forma de diccionario
+
     def obtener_recompensa(self, estado):
 
         for destino in self.destinos:
@@ -135,10 +140,6 @@ class Estado:
     def __eq__(self, otro):
         return (self.fila, self.columna) == (otro.fila, otro.columna)
 
-    # Usado para comprobar si un estado es o no destino
-    def __hash__(self):
-        return hash((self.fila, self.columna))
-
     def __repr__(self):
         return str(f"({self.fila},{self.columna})")
 
@@ -153,7 +154,7 @@ class Agente:
         3: "LEFT"
     }
 
-    def __init__(self, entorno, alpha, gamma, epsilon, decaimiento_epsilon):
+    def __init__(self, entorno, alpha, gamma, epsilon, decaimiento_epsilon, json_dir):
         self.entorno = entorno
         self.movimientos = [Agente.direcciones[movimiento] for movimiento in
                             Agente.mov_numericos]  # Arriba, Derecha, Abajo, Izquierda respectivamente
@@ -162,6 +163,9 @@ class Agente:
         self.gamma = gamma
         self.epsilon = epsilon
         self.decaimiento_epsilon = decaimiento_epsilon
+        self.politica = None
+        self.json_dir = json_dir
+        self.tiempo_ejecucion = None
 
     # El agente sólo desea aplicar una accion en base a un estado, es el entorno el encargado de que esa aplicacion se ejecute
     # así como se obtenga la recompensa
@@ -170,17 +174,20 @@ class Agente:
         nuevo_estado, recompensa = self.entorno.mover_agente(accion_elegida, estado_actual, explorar)
         return nuevo_estado, recompensa
 
+    # Se selecciona el máximo valor para un estado en la Q tabla asociada a una accion de las 4 posibles en cada nodo
     def max_q(self, estado):
         posibles_acciones = [self.qtabla[estado.fila][estado.columna][accion] for accion in self.mov_numericos]
         aux = posibles_acciones
         for accion in range(len(aux)):
             if not self.entorno.es_accion_valida(accion, estado):
                 # Con esto, descartamos los índices que llevan a acciones no válidas
+                # Sin esto el agente se dispersa mucho
                 aux[accion] = np.min(self.entorno.recompensas_finales) - 50
         indices_maximos = np.where(aux == np.max(aux))[0]
         accion_elegida = np.random.choice(indices_maximos)  # En caso de tener varios índices iguales
         return accion_elegida
 
+    # Aplicación directa de la fórmula de los apuntes, se encarga de actualizar la Q Tabla
     def actualizar_qtabla(self, estado_actual, accion_elegida, recompensa, nuevo_estado, destino):
         if destino:
             nuevo_valor = (1.0 - self.alpha) * self.qtabla[estado_actual.fila][estado_actual.columna][
@@ -195,28 +202,30 @@ class Agente:
             self.qtabla[estado_actual.fila][estado_actual.columna][accion_elegida] = nuevo_valor
         return nuevo_valor
 
+    # Ejecución del algoritmo en función del número de episodios
     def ejecutar_algoritmo(self, num_episodios):
+        self.tiempo_ejecucion = time()
         for i in range(num_episodios):
             self.episodio()
+        self.tiempo_ejecucion = time() - self.tiempo_ejecucion
         return self.obtener_politica()
 
+    # Método encargado de aplicar un episodio, finaliza cuando llega a un estado de meta y actualiza la Q Tabla
+    # Aplicación directa del pseucocódigo de los apuntes
     def episodio(self):
         estado_actual = self.entorno.inicio
         destino = False
         while not destino:
-            # print("-------------------")
-            # print(f"Estado actual {estado_actual}")
             accion_elegida = self.max_q(estado_actual)
-            # print(f"Accion elegida {self.movimientos[accion_elegida]}")
             nuevo_estado, recompensa = self.realizar_movimiento(accion_elegida, estado_actual)
-            # print(f"Nuevo estado {nuevo_estado}")
-            # print(f"Recompensa {recompensa}")
             if self.entorno.es_destino(nuevo_estado):
                 destino = True
                 self.epsilon *= self.decaimiento_epsilon  # Decaimiento de épsilon en cada final de episodio
             self.actualizar_qtabla(estado_actual, accion_elegida, recompensa, nuevo_estado, destino)
             estado_actual = nuevo_estado
 
+    # Con esto obtenemos la política dentro de todos los estados posibles que no sean ni destinos ni bloqueados
+    # En función de los valores máximos de la Q Tabla en cada nodo
     def obtener_politica(self):
         politica = {}
         estados_posibles = [(fila, columna) for fila in range(self.entorno.filas) for columna in
@@ -224,13 +233,37 @@ class Agente:
         for estado in estados_posibles:
             if estado not in self.entorno.destinos and estado not in self.entorno.peligros_fatales and estado not in self.entorno.bloqueados:
                 politica[estado[0], estado[1]] = self.direcciones.get(self.max_q(Estado(estado[0], estado[1])))
-        print(self.qtabla)
+        self.politica = politica
+        self.estadisticas()
         return politica
+
+    def estadisticas(self):
+        destinos = []
+        for estados in self.entorno.destinos.keys():
+            destinos.append(estados)
+
+        print("----ALGORITMO QLEARNING----")
+        print("\n ")
+        print(f"Estado inicial: {self.entorno.inicio}")
+        print(f"Personas a rescatar: {destinos}")
+        print(f"Tamaño de la ciudad: {self.entorno.filas} x {self.entorno.columnas}")
+        print(f"Tiempo de ejecución del Algoritmo: {self.tiempo_ejecucion:.2f} segundos")
+        print("\n ")
+        print("----QTabla----")
+        print("\n ")
+        print(self.qtabla)
+        print("\n ")
+        print("----POLÍTICA OBTENIDA CON QQLEARNING----")
+        for estado, accion in self.politica.items():
+            print(f"\"({estado[0]}, {estado[1]})\": \"{accion}\"")
+        print("----MAPA DE LA POLÍTICA OBTENIDA QQLEARNING----")
+        print("\n ")
+        draw_policy_map(json_dir, self.politica)
 
 
 if __name__ == '__main__':
     # Inicializamos el problema cargando el json y parametrizandolo
-    penalizacion_entorno = -0.04
+    penalizacion_entorno = -0.1
     penalizacion_peligro = -5
     estocasticidad_entorno = 0.8
     numero_episodios = 1000
@@ -239,15 +272,17 @@ if __name__ == '__main__':
     epsilon = 0.2
     decaimiento_epsilon = 0.05
 
+    # Parámetro necesario para dibujar la política
+    json_dir = os.path.join(os.getcwd(), './initial-rl-instances/lesson5-rl.json')
+    # Definimos el entorno con las variables parametrizadas
     entorno = Entorno('./initial-rl-instances/lesson5-rl.json', penalizacion_entorno, penalizacion_peligro,
                       estocasticidad_entorno)
-    algoritmo = Agente(entorno, alpha, gamma, epsilon, decaimiento_epsilon)
+    # Inicializamos el Algoritmo
+    algoritmo = Agente(entorno, alpha, gamma, epsilon, decaimiento_epsilon, json_dir)
 
-    json_dir = os.path.join(os.getcwd(), './initial-rl-instances/lesson5-rl.json')
+    # Ejecutamos el Algoritmo con el nº de episodios y obtenemos la política y la imprimimos
+    # Posteriormente la dibujamos
+    algoritmo.ejecutar_algoritmo(numero_episodios)
 
-    politica = algoritmo.ejecutar_algoritmo(numero_episodios)
 
-    for estado, accion in politica.items():
-        print(f"\"({estado[0]}, {estado[1]})\": \"{accion}\"")
 
-    draw_policy_map(json_dir, politica)
